c576f46e1fd04d3e30936c85e0d75ea5a0b7629e
Manohar <manoharreddy.koppala@gmail.com>
2025-04-01 16:45:58 +0530

    update theme

--- frontend/src/components/ui/gridBackground.jsx
+++ frontend/src/components/ui/gridBackground.jsx
@@ -4,17 +4,15 @@ import React from "react";
 export function GridSmallBackgroundDemo({ children }) {
   return (
     <div
-    className='w-full bg-black text-white bg-grid-white/[0.2] relative'>
+      className="w-full bg-black text-white bg-grid-white/[0.2] relative">
       <div
         className={cn(
           "absolute inset-0",
           "[background-size:20px_20px]",
-          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+          "[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
         )} />
       {/* Radial gradient for the container to give a faded look */}
-      <div
-        className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="relative z-20">
         {children}
       </div>
