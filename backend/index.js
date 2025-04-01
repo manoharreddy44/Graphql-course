6b6299fca6b63e66b4770d50cca5d28083bc8483
Manohar <manoharreddy.koppala@gmail.com>
2025-04-01 16:08:22 +0530

    cron job added

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,12 @@ import mergedTypeDefs from "./typeDefs/index.js";
 import { connectDB } from "./db/connectDB.js";
 import { configurePassport } from "./passport/passport.config.js";
 import path from "path";
+import job from "./cron.js";
 
 dotenv.config();
 configurePassport();
+
+job.start();
 const __dirname = path.resolve();
 
 const app = express();
